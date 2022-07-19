e71df4327e8f3a58acd6bda63628dbe18db8b811
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-19 13:19:40 -0600

    added login and linter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { toHaveErrorMessage } from '@testing-library/jest-dom/dist/matchers'
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,24 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
+  // implement a login function. token returned state in apps stater user
   return (
     <div>
       <h2>blogs</h2>
+
+      <Notification message = {errorMessage} />
+
+      <form onSubmit={handleLogin}>
+    
+
+
+      </form>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
