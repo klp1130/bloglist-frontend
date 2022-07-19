c72b1cd732b9f45b26669df875da690155b4a929
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-19 14:02:33 -0600

    handling login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
   const [username, setUserName] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,10 +17,23 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUserName('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong Credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
   }
+}
 
   // implement a login function. token returned state in apps stater user
   return (
