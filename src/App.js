20515d9b488d7596625b82ebe16b06ac8feb2074
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-22 06:28:38 -0600

    minor changes to app.js

--- src/App.js
+++ src/App.js
@@ -89,7 +89,9 @@ const App = () => {
         <button onClick={logout}>logout</button>
         <br></br>
 
-        <h1>create new</h1>
+        <h2>create new</h2>
+
+        <h2>your blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
